35c450328de227a485b2a89d7b7d6e4b86fb5fb7
mattperkins <github@mattperkins.com>
2018-09-16 12:11:06 +0100

    add dummy data for create job

--- app/Controllers/Http/JobController.js
+++ app/Controllers/Http/JobController.js
@@ -3,8 +3,15 @@
 const Job = use('App/Models/Job')
 
 class JobController {
-    async home({view})
-    
+    async home({view}) {
+     
+    // create a job
+    const job = new Job
+      job.title = "Head of Awesome"
+      job.link = "http://dormshed.com"
+      job.description = "Doing amazing stuff daily"
+    await job.save()
+  }
 }
 
 module.exports = JobController
