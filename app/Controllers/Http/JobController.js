90328f183d05cb4c4895f39fade8587b39953f7a
mattperkins <github@mattperkins.com>
2018-09-16 12:34:13 +0100

    remove test render job data

--- app/Controllers/Http/JobController.js
+++ app/Controllers/Http/JobController.js
@@ -5,12 +5,13 @@ const Job = use('App/Models/Job')
 class JobController {
     async home({view}) {
      
-    // create test render job data
-    const job = new Job
-      job.title = "Head of Awesome"
-      job.link = "http://dormshed.com"
-      job.description = "Doing amazing stuff daily"
-    await job.save()
+   // create test render job data
+    // const job = new Job
+    //  job.title = "Head of Awesome"
+    //  job.link = "http://dormshed.com"
+    //  job.description = "Doing amazing stuff daily"
+    // await job.save()
+
 
     // fetch a job
     const jobs = await Job.all()
