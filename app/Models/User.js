547bc5f74e2a57637330933fa2d8a9cf7540b972
mattperkins <github@mattperkins.com>
2018-09-18 00:27:30 +0100

    add jobs mv and validator

--- app/Models/User.js
+++ app/Models/User.js
@@ -18,6 +18,14 @@ class User extends Model {
     })
   }
 
+  jobs() {
+    return this.hasMany('App/Models/Job')
+}
+
+
+
+
+
   /**
    * A relationship on tokens is required for auth to
    * work. Since features like `refreshTokens` or
